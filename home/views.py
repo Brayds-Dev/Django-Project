11c6f3e377574dab29aa7f566a1b802d8e8de8b8
Brayds_Dev <braydendawson91@gmail.com>
2022-07-05 14:17:52 +1200

    Add item list view

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
+from .models import Item
 # Create your views here.
+
+class ItemListView(ListView):
+    model = Item
+    template_name = "home/home.html"
