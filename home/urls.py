b0c7ba545728c8c4b652c607a9a7dcd093a9cbad
Brayds_Dev <braydendawson91@gmail.com>
2022-07-05 15:11:51 +1200

    Basic detail view

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ItemListView
+from .views import ItemListView, ItemDetailView
 
 urlpatterns = [
     path("", ItemListView.as_view(), name="home"),
+    path("items/<int:pk>/", ItemDetailView.as_view(), name="item_detail"),
 ]
