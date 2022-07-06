827bffcd08d7e0c889a7ab8e277ea09bda44e5cb
Brayds_Dev <braydendawson91@gmail.com>
2022-07-06 13:46:24 +1200

    Adding edit, delete views

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import ItemListView, ItemDetailView, ItemCreateView
+from .views import ItemListView, ItemDetailView, ItemCreateView, ItemDeleteView, ItemUpdateView
 
 urlpatterns = [
-    path("", ItemListView.as_view(), name="home"),
-    path("items/<int:pk>/", ItemDetailView.as_view(), name="item_detail"),
     path("items/new/", ItemCreateView.as_view(), name="item_new"),
+    path("items/<int:pk>/", ItemDetailView.as_view(), name="item_detail"),
+    path("items/<int:pk>/edit/", ItemUpdateView.as_view(), name="item_edit"),
+    path("items/<int:pk>/delete/", ItemDeleteView.as_view(), name="item_delete"),
+    path("", ItemListView.as_view(), name="home"),
+    
+    
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Item
 # Create your views here.
@@ -14,11 +15,31 @@ class ItemDetailView(DetailView):
 
     # Will be adding comment context data here for adding comments
 
-class ItemCreateView( CreateView):
+class ItemCreateView(LoginRequiredMixin, CreateView):
     model = Item
     template_name = "home/item_new.html"
     fields = ["title", "description", "price", "condition"]
+    success_url = reverse_lazy("home")
 
     def form_valid(self, form):
-        form.instance.author = self.request.user
+        form.instance.user = self.request.user
         return super().form_valid(form)
+
+class ItemUpdateView(LoginRequiredMixin, UpdateView):
+    model = Item
+    template_name = "home/item_edit.html"
+    fields = ["title", "description", "price", "condition"]
+    
+
+    def test_func(self):  # new
+        obj = self.get_object()
+        return obj.user == self.request.user
+
+class ItemDeleteView(LoginRequiredMixin, DeleteView):
+    model = Item
+    template_name = "home/item_delete.html"
+    success_url = reverse_lazy("home")
+
+    def test_func(self):  # new
+        obj = self.get_object()
+        return obj.user == self.request.user
